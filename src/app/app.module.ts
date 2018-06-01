a728a6575d217273e99bc2286f75e2c34fc64b40
Sekar, Sindhuja (ITCDEC) - KLM <Sindhuja.Sekar@klm.com>
2018-06-01 16:21:26 +0530

    Updating google authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule } from '@agm/core';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
+import { NgxErrorsModule } from '@ultimate/ngxerrors';
+import { BrowserModule } from '@angular/platform-browser';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicApp, IonicErrorHandler, IonicModule, Config } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { firebaseConfig } from '../config';
+import { LoginPage } from '../pages/login/login'
+import { AuthService} from '../services/auth.service'
+
+
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+		IonicModule.forRoot(MyApp),
+		AgmCoreModule.forRoot(),
+
+    AngularFireModule.initializeApp(firebaseConfig.fire),
+
+    
+		NgxErrorsModule
+		
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
-    StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    StatusBar,   
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService,
+    AngularFireAuth
   ]
 })
 export class AppModule {}
---
+++ src/config.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+export const firebaseConfig = {
+	fire: {
+	apiKey: "AIzaSyAxNqjwHGO2F55w8TcKxA4cN29UydZxcFc",
+    authDomain: "new-proj-c22ed.firebaseapp.com",
+    databaseURL: "https://new-proj-c22ed.firebaseio.com",
+    projectId: "new-proj-c22ed",
+    storageBucket: "new-proj-c22ed.appspot.com",
+    messagingSenderId: "560352760730"
+	}
+};
